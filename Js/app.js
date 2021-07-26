791b60a8bc561a518a3f6328efa3fba95813169e
Rand92 <s.rund@yahoo.com>
2021-07-26 18:59:13 +0300

    updating index.html,app.js,style.css

---
+++ Js/app.js
@@ -0,0 +1,63 @@
+'use strict' ;
+
+let welcome = prompt('what is your name?');
+alert ('welcome to my website dear ' + welcome);
+
+
+let q1 = prompt('my first name is Rand,right?').toLowerCase();
+switch(q1) {
+    case 'yes':
+        // console.log('yes');
+        alert(q1);
+        break ;
+    case 'no' :
+        // console.log('no');
+        alert(q1);
+        break ;
+}
+let q2 = prompt('am i 30 year-old?').toLowerCase();
+switch(q2) {
+    case 'yes':
+        // console.log('yes');
+        alert(q2);
+        break ;
+    case 'no' :
+        // console.log('no');
+        alert(q2);
+        break ;
+}
+let q3 = prompt('am i a civil engineer?').toLowerCase();
+switch(q3) {
+    case 'yes':
+        // console.log('yes');
+        alert(q3);
+        break ;
+    case 'no' :
+        // console.log('no');
+        alert(q3);
+        break ;
+}
+let q4 = prompt("i've graduated from AAU ,is that right?").toLowerCase();
+switch(q4) {
+    case 'yes':
+        // console.log('yes');
+        alert(q4);
+        break ;
+    case 'no' :
+        // console.log('no');
+        alert(q4);
+        break ;
+}
+let q5 = prompt('i have a 5 years of work experience ,yes?').toLowerCase();
+switch(q5) {
+    case 'yes':
+        // console.log('yes');
+        alert(q5);
+        break ;
+    case 'no' :
+        // console.log('no');
+        alert(q5);
+        break ;
+}
+
+alert ('Hope you enjoyed  ' + welcome + '  stay safe ^_^');
