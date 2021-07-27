35e302091b3ba7abb22ef8c8fa03b0bd20dc0173
Rand92 <s.rund@yahoo.com>
2021-07-27 19:37:03 +0300

    adding q6 & q7

--- Js/app.js
+++ Js/app.js
@@ -1,7 +1,7 @@
-'use strict' ;
-
+'use strict';
+let score = 0
 let username = prompt('what is your name?');
-alert ('welcome to my website dear ' + username);
+alert('welcome to my website dear ' + username);
 
 
 let q1 = prompt('my first name is Rand,right?').toLowerCase();
@@ -10,6 +10,7 @@ switch(q1) {
     case 'y' :
         // console.log('yes');
         alert('yes!That is right!');
+        score++
         break ;
     case 'no' :
     case 'n' :
@@ -28,6 +29,7 @@ switch(q2) {
     case 'n' :
         // console.log('no');
         alert('that is right ,i am 28');
+        score++
         break ;
 }
 let q3 = prompt('am i a civil engineer?').toLowerCase();
@@ -36,6 +38,7 @@ switch(q3) {
     case 'y' :
         // console.log('yes');
         alert('yes! i am');
+        score++
         break ;
     case 'no' :
     case 'n' :
@@ -49,6 +52,7 @@ switch(q4) {
     case 'y' :
         // console.log('yes');
         alert('yes!');
+        score++
         break ;
     case 'no' :
     case 'n' :
@@ -67,7 +71,55 @@ switch(q5) {
     case 'n' :
         // console.log('no');
         alert('i worked for 3 years');
+        score++
         break ;
 }
+let correctNum = 20;
+let q6 = parseInt(prompt('lets play a fun game!, guess a number?'));
+let counter = 0
+for (let i = 0; i < 3; i++) {
+    if (q6 < correctNum) {
+        alert('too low'); counter++
+        q6 = parseInt(prompt('lets play a fun game!, guess a number?'));
+    }
+    else if (q6 > correctNum) {
+        counter++
+        alert('too high'); q6 = parseInt(prompt('lets play a fun game!, guess a number?'));
+    }
+    else if (q6 === correctNum) {
+        alert('you guessed right!');
+        score++
+        break;
+    }
+}
+ if (counter ==3) { alert('wrong guessing ,see you next time! the correct answer is  ' + correctNum) };
 
-alert ('Hope you enjoyed  ' + username + '  stay safe ^_^');
+let favmovie = ['the vow', 'you and me', 'beautiful mind', 'the hunger game'];
+let count = 6
+
+for (let i = 0 ; i < 6; i++) {
+    let q7 = prompt('guess one of my fav movies?[the vow, you and me, beautiful mind, the hunger game]').toLowerCase();
+    document.write(q7)
+    if (count == 0) {
+        alert('good job'); count--
+        break;
+    }
+    for (let b = 0; b < favmovie.length; b++) {
+        if (q7 == favmovie[b]) {
+            alert('correct answer')
+        i = 6 ;
+        score++
+            break;
+        }
+        else if (b == favmovie.length - 1) { alert('incorrect answer') }
+
+    }
+
+}
+alert ( 'the correct answers the vow, beautiful mind') ;
+
+
+alert ('your score is  '+ score);
+
+
+alert('Hope you enjoyed  ' + username + '  stay safe ^_^');
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # guessing-game
+ > by Rand Albqour *[Resume](https://docs.google.com/document/d/1bG2mfvPs_5_iwDhFwgwy0apPQYfdZw3BxBkdOTENFmU/edit#heading=h.sbziogryzzql)*
+
+***About me*** project or ***guessing-game*** project is about the webpage creater ! 
+### Contains :
+- Three directories are :
+1. CSS  
+2. JavaScript
+3. img 
+-  .eslintrc.json File
+- .gitignore File
+- LICENSE File
+-  README.md File
+- index.html File
